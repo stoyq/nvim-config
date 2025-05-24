c855b58681f4e10ac735e9695754f590b387efed
Johnson <stoiqed@gmail.com>
2025-05-24 22:53:11 +0800

    Add <leader>r mappings for Python and R to LazyVim config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,27 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+
+-- <leader>rp for Python scripts
+vim.keymap.set("n", "\\rp", function()
+  vim.cmd("w")
+  vim.cmd("split")
+  vim.cmd("resize 15")
+  -- Pause after execution
+  local cmd = "terminal python3 " .. vim.fn.expand("%") .. "; read -n 1 -s -r -p 'Press any key to continue...'"
+  vim.cmd(cmd)
+end, { desc = "Run Python with pause" })
+
+
+
+vim.keymap.set("n", "<leader>s.", function()
+  require("telescope.builtin").live_grep({ cwd = vim.fn.expand("%:p:h") })
+end, { desc = "Live Grep (current dir)" })
+
+
+-- <leader>rr for Rscripts
+vim.keymap.set("n", "\\rr", function()
+  vim.cmd("w")
+  vim.cmd("split")
+  vim.cmd("resize 15")
+  -- Pause after execution
+  local cmd = "terminal Rscript " .. vim.fn.expand("%") .. "; read -n 1 -s -r -p 'Press any key to continue...'"
+  vim.cmd(cmd)
+end, { desc = "Run Rscript with pause" })
